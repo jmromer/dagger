89d16da763f55da64ee2183a9faa0f1c36bff892
Jake Romer <jkrmr.io@gmail.com>
2017-07-09 16:41:53 -0400

    Add first sample test with implementation

--- lib/dependency_resolver.rb
+++ lib/dependency_resolver.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class DependencyResolver
-  def hello
+  PACKAGE_ENTRY_FORMAT = /([[:word:]]+): ([[:word:]]+)?/i
+
+  def resolve(list)
+    list = list.each_with_object([]) do |entry, memo|
+      memo << entry.scan(PACKAGE_ENTRY_FORMAT).flatten.map(&:to_s)
+    end
+
+    sorted = list.sort_by(&:last)
+
+    sorted.map(&:first).join(", ")
   end
 end
--- spec/dependency_resolver_spec.rb
+++ spec/dependency_resolver_spec.rb
@@ -4,5 +4,17 @@ require "spec_helper"
 require "dependency_resolver"
 
 RSpec.describe DependencyResolver do
-  it { is_expected.to respond_to(:hello) }
+  it { is_expected.to respond_to(:resolve) }
+
+  describe "#resolve" do
+    context "given a valid list of dependencies" do
+      it "returns a comma-separated string of sorted dependencies" do
+        list = ["KittenService: CamelCaser", "CamelCaser: "]
+
+        sorted_list = described_class.new.resolve(list)
+
+        expect(sorted_list).to eq "CamelCaser, KittenService"
+      end
+    end
+  end
 end
