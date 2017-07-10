c0553496d86a21ec45ad09db12d5df56afeceae6
Jake Romer <jkrmr.io@gmail.com>
2017-07-09 21:21:34 -0400

    Pass second test, extract Package class

--- lib/dependency_resolver.rb
+++ lib/dependency_resolver.rb
@@ -1,15 +1,58 @@
 # frozen_string_literal: true
 
-class DependencyResolver
+class Package
+  attr_reader :name, :dependency_name
+  attr_accessor :dependency
+
   PACKAGE_ENTRY_FORMAT = /([[:word:]]+): ([[:word:]]+)?/i
 
+  def self.parse_list(strings:)
+    strings.each_with_object([]) do |entry, memo|
+      name, dep_name = entry.scan(PACKAGE_ENTRY_FORMAT).flatten
+      memo << Package.new(name, dep_name)
+    end
+  end
+
+  def initialize(name, dependency_name)
+    @name = name
+    @dependency_name = dependency_name
+  end
+
+  def inspect
+    return name if dependency.nil?
+    "#{name} <#{dependency_name}>"
+  end
+
+  def to_s
+    name
+  end
+end
+
+class DependencyResolver
   def resolve(list)
-    list = list.each_with_object([]) do |entry, memo|
-      memo << entry.scan(PACKAGE_ENTRY_FORMAT).flatten.map(&:to_s)
+    packages = Package.parse_list(strings: list)
+    graph = build_graph(packages)
+
+    sorted_list = graph.each_with_object([]) do |package, memo|
+      memo.push(*gather_dependencies(package))
     end
 
-    sorted = list.sort_by(&:last)
+    sorted_list.uniq.join(", ")
+  end
 
-    sorted.map(&:first).join(", ")
+  def build_graph(list)
+    list.each do |package|
+      dependency = list
+                   .select { |dep| dep.name == package.dependency_name }
+                   .first
+      package.dependency = dependency
+    end
+  end
+
+  def gather_dependencies(package, list = [])
+    list.unshift(package)
+    return list if package.dependency.nil?
+
+    gather_dependencies(package.dependency, list)
   end
 end
--- spec/dependency_resolver_spec.rb
+++ spec/dependency_resolver_spec.rb
@@ -7,13 +7,33 @@ RSpec.describe DependencyResolver do
   it { is_expected.to respond_to(:resolve) }
 
   describe "#resolve" do
-    context "given a valid list of dependencies" do
+    context "given a list with a single dependency" do
       it "returns a comma-separated string of sorted dependencies" do
         list = ["KittenService: CamelCaser", "CamelCaser: "]
 
         sorted_list = described_class.new.resolve(list)
 
         expect(sorted_list).to eq "CamelCaser, KittenService"
+      end
+    end
+
+    context "given a list with multiple dependencies" do
+      it "returns a comma-separated string of sorted dependencies" do
+        list = ["KittenService: ",
+                "Leetmeme: Cyberportal",
+                "Cyberportal: Ice",
+                "CamelCaser: KittenService",
+                "Fraudstream: Leetmeme",
+                "Ice: "]
+
+        sorted_list = described_class.new.resolve(list)
+
+        expect(sorted_list).to eq %w(KittenService
+                                     Ice
+                                     Cyberportal
+                                     Leetmeme
+                                     CamelCaser
+                                     Fraudstream).join(", ")
       end
     end
   end
